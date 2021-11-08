5a50f4165c68d8c8f373b2f66a1ffe4d13d5fc11
VectorTensor <comppro54@gmail.com>
2021-11-08 12:35:27 +0545

    finished adding most stuffs some CSS styling remaining

--- frontend/src/Project.js
+++ frontend/src/Project.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link  } from 'react-router-dom';
 
 function Project()
 {
@@ -9,7 +10,15 @@ function Project()
             Project
         </h1>
         <div className="description">
-
+    Here is some information of the project that i have done.
+        <ol>
+        <li><a href="https://github.com/VectorTensor/Chad-anime-downloader">Anime downloader</a></li>
+        <li><a href="https://github.com/VectorTensor/ProjectManager">To do list</a></li>
+        <li><a href="https://github.com/VectorTensor/MemoryGame">Memory game using pygame.</a></li>
+        <li><a href ="https://github.com/VectorTensor/typing">Typing widget</a></li>
+        <li><a href ="https://github.com/VectorTensor/ProjectManager">To do list</a></li>
+        <li><a href="https://github.com/VectorTensor/personal-website">Personal website</a></li>
+        </ol>
         </div>
         </div>
     )
