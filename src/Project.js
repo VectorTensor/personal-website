98a406d80fa420c162c37cb87487ada083757bb3
VectorTensor <comppro54@gmail.com>
2022-05-31 11:25:25 +0545

    updated website #1

--- src/Project.js
+++ src/Project.js
@@ -12,12 +12,21 @@ function Project()
         <div className="description">
     Here is some information of the project that i have done.
         <ol>
-        <li><a href="https://github.com/VectorTensor/Chad-anime-downloader">Anime downloader</a></li>
-        <li><a href="https://github.com/VectorTensor/ProjectManager">To do list</a></li>
-        <li><a href="https://github.com/VectorTensor/MemoryGame">Memory game using pygame.</a></li>
-        <li><a href ="https://github.com/VectorTensor/typing">Typing widget</a></li>
-        <li><a href ="https://github.com/VectorTensor/ProjectManager">To do list</a></li>
+        <li> <a href="https://github.com/VectorTensor/IOEOverflow"> IOE Overflow</a></li>
+        Its a website for IOE students to peer review their answers. It has elastic search facility along with a login system and also OCR so that the student can search question even
+        from the screenshots.
         <li><a href="https://github.com/VectorTensor/personal-website">Personal website</a></li>
+        Its a website that has details about me and work I have done. Its completely made in reactjs.
+        <li><a href="https://github.com/VectorTensor/Chad-anime-downloader">Anime downloader</a></li>
+        Its a anime downloading script that automatically downloads the anime you want and the episodes specified.
+
+        <li><a href="https://github.com/VectorTensor/ProjectManager">To do list</a></li>
+        App to make to do list in command line.
+        
+        <li><a href="https://github.com/VectorTensor/MemoryGame">Memory game using pygame.</a></li>
+        Simple game made using pygame.
+        <li><a href ="https://github.com/VectorTensor/typing">Typing widget</a></li>
+        A widget to measure typing speed
         </ol>
         </div>
         </div>
