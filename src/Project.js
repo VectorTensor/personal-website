c4b1b60792d5ef7ad90dd4fb4d36fcc942086a18
VectorTensor <comppro54@gmail.com>
2022-06-09 15:38:29 +0545

    update project #1

--- src/Project.js
+++ src/Project.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Link  } from 'react-router-dom';
 
+import login from './images/login.JPG'
+import post from './images/posts.JPG'
+import forum from './images/forum.JPG'
+import search_correct from './images/search_correct.JPG'
+
+import  search_mistake from './images/search_mistake.JPG'
+
+import  search_image from './images/search_image.JPG'
 function Project()
 {
     return(
@@ -14,7 +22,37 @@ function Project()
         <ol>
         <li> <a href="https://github.com/VectorTensor/IOEOverflow"> IOE Overflow</a></li>
         Its a website for IOE students to peer review their answers. It has elastic search facility along with a login system and also OCR so that the student can search question even
-        from the screenshots.
+        from the screenshots images.
+        here are some screenshots of the project
+        <a href={login}>
+<img src={login} className="photos"/>
+        </a>
+        <p> This is the login screen where we can login to the account. If you don't have account you can still view the post but you cant add new posts and upvote/downvote or comment.
+        You can click on Don't have an account yet to create a new account.
+        </p>
+        <a href={post}>
+<img src={post} className="photos"/>
+        </a>
+        <p> This is posts page where you can view recent posts </p>
+
+        <a href={forum}>
+<img src={forum} className="photos"/>
+        </a>
+        <p>This is page where you can add new post but the user must be logged in </p>
+        <a href={search_correct}>
+<img src={search_correct} className="photos"/>
+        </a>
+        <p>This is the screen shot of search</p> 
+        <a href={search_mistake}>
+<img src={search_mistake} className="photos"/>
+        </a>
+        <p> This is search when there is a mistake in spelling. Elastic search still manages to get the correct result.</p>
+
+        <a href={search_image}>
+<img src={search_image} className="photos"/>
+        </a>
+<p> This is search using text in the image. Single we have used OCR we can also search for the terms in the image.</p>
+
         <li><a href="https://github.com/VectorTensor/personal-website">Personal website</a></li>
         Its a website that has details about me and work I have done. Its completely made in reactjs.
         <li><a href="https://github.com/VectorTensor/Chad-anime-downloader">Anime downloader</a></li>
